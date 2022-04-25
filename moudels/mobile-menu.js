b62e07efd5118825fc3167054288e316381a1e47
unknown <ibr.s.ahmat@gmail.com>
2022-04-26 02:44:02 +0300

    Create moudels directory containes some seprated Java Script moudles

---
+++ moudels/mobile-menu.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('.navigation');
+const listGroup = document.querySelector('.right');
+const openMenu = document.querySelector('#menuBarIcon');
+const closeMenu = document.querySelector('#closeMenuIcon');
+
+export {
+  nav, listGroup, openMenu, closeMenu,
+};
---
+++ moudels/nav-list.js
@@ -0,0 +1,9 @@
+// sections
+const navLinks = document.querySelectorAll('.right-list');
+const listSection = document.querySelector('#books-list');
+const addBookSection = document.querySelector('#add-new');
+const contactSection = document.querySelector('#contact-us');
+
+export {
+  navLinks, listSection, addBookSection, contactSection,
+};
